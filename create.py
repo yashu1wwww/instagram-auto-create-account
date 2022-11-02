4002bbc02e41cae3fc2b77283b4d2d06941f6c57
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-11-02 12:57:16 +0530

    Update create.py

--- create.py
+++ create.py
@@ -9,30 +9,45 @@ import accountInfoGenerator as account
 browser = webdriver.Chrome()
 browser.get("https://www.instagram.com/accounts/emailsignup/")
 time.sleep(5)
-#name = account.username()
-#time.sleep(3)
-browser.find_element_by_name('emailOrPhone').send_keys("ugi07248@nezid.com") #replace with your mail/10 minutes mail/temp mail/phone no
+browser.find_element_by_name('emailOrPhone').send_keys("gxdbfamctreq@spacehotline.com") #replace with your mail/temp mail or 10minutes mail
 time.sleep(2) 
 # Fill the fullname value
 fullname_field = browser.find_element_by_name('fullName')
 fullname_field.send_keys(account.generatingName())
 username_field = browser.find_element_by_name('username')
 username_field.send_keys(account.username())
-time.sleep(10)
-#you must enter password and click next button because of part process you cant remember the password so you have 10 second time for it so you remember it and remaining things go automated
+time.sleep(11)
+
+#for password leaved 11 seconds so you must enter your required password in 11 second
+
+input = browser.find_element_by_xpath('/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div[7]/div/button').click()
+time.sleep(2)
+
 #Birthday verification
-browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[1]/select").click()
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[1]/select/option[4]"))).click()
+browser.find_element_by_xpath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div/div[4]/div/div/span/span[1]/select").click()
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div/div[4]/div/div/span/span[1]/select/option[4]"))).click()
 
-browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[2]/select").click()
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[2]/select/option[10]"))).click()
+browser.find_element_by_xpath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div/div[4]/div/div/span/span[2]/select").click()
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div/div[4]/div/div/span/span[2]/select/option[10]"))).click()
 
-browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select").click()
-WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select/option[27]"))).click()
+browser.find_element_by_xpath("/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select").click()
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select/option[29]"))).click()
+time.sleep(1)
 
-WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[6]/button"))).click()
+input = browser.find_element_by_xpath('/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div/div[6]/button').click()
 time.sleep(50)
 driver.close()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
